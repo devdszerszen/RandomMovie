4db224df27a020b1bbec35997925b1308d56a24e
devdszerszen <dev.dszerszen@gmail.com>
2019-08-23 13:17:17 +0200

    23/08 added genre info

--- app/src/main/java/pl/dszerszen/randommovie/DetailsActivity.java
+++ app/src/main/java/pl/dszerszen/randommovie/DetailsActivity.java
@@ -6,9 +6,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -22,6 +26,9 @@ public class DetailsActivity extends AppCompatActivity {
     @BindView(R.id.details_desc) TextView desc;
     @BindView(R.id.details_poster) ImageView posterView;
     @BindView(R.id.details_layout) ConstraintLayout detailsLayout;
+    @BindView(R.id.details_genres_layout) LinearLayout genresLayout;
+    @BindView(R.id.details_time_layout) LinearLayout timeLayout;
+    @BindView(R.id.details_time_value) TextView timeValue;
     ActionBar actionBar;
 
     SingleMovieDetails movie;
@@ -33,9 +40,17 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         ButterKnife.bind(this);
         actionBar = getSupportActionBar();
+        setBackground();
         api = new Api();
         getMovieData();
         showMovieInfo();
+
+
+    }
+
+    public void setBackground() {
+        View root = timeValue.getRootView();
+        root.setBackgroundColor(Color.BLACK);
     }
 
     public void getMovieData() {
@@ -54,6 +69,18 @@ public class DetailsActivity extends AppCompatActivity {
         title.setText(movie.title);
         desc.setText(movie.overview);
         detailsLayout.bringChildToFront(title);
+
+        for (int i = 0; i<movie.genres.size(); i++) {
+            TextView tmpTxtView = new TextView(this);
+            tmpTxtView.setTextColor(Color.WHITE);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1f);
+            tmpTxtView.setLayoutParams(params);
+            tmpTxtView.setTextSize(14f);
+            tmpTxtView.setGravity(Gravity.CENTER);
+            tmpTxtView.setText(movie.genres.get(i).name.toLowerCase());
+            genresLayout.addView(tmpTxtView);
+        }
+        timeValue.setText(String.valueOf(movie.runtime)+ " min");
         }
     }
 }
